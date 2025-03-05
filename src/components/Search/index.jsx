1e02c332c854f8591c0041d6f411ddd29f367ae8
Rushab Khadka <Rushabkhadka67@gmail.com>
2025-03-05 12:19:35 +0545

    Modified

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import "../Search/style.css"
 
 const Search = () => {
   return (
-    <div className='searchBox w-[100%] h-[50px] bg-[#e5e5e5] rounded-[20px]'>
-      
+    <div className='searchBox w-[100%] h-[50px] bg-[#e5e5e5] rounded-[20px] relative p-2'>
+        <input type="text" placeholder='Search for Products.....' className='w-full h-[35px] 
+        focus:outline-none bg-inherit p-2 text-[15px]' />
     </div>
   )
 }
