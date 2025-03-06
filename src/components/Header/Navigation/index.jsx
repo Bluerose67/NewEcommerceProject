2e2a2d8fd21229f59cea412289000b909ca2de8e
Rushab Khadka <Rushabkhadka67@gmail.com>
2025-03-06 16:25:20 +0545

    Nav Bar completed

---
+++ src/components/Header/Navigation/index.jsx
@@ -0,0 +1,88 @@
+import React from 'react'
+import Button from '@mui/material/Button';
+import { RiMenu2Line } from "react-icons/ri";
+import { LiaAngleDownSolid } from "react-icons/lia";
+import { Link } from "react-router-dom" 
+import { GoRocket } from "react-icons/go";
+
+
+
+
+const Navigation = () => {
+  return (
+    <nav className='py-2'>
+        <div className="container flex items-center justify-end gap-8">
+            <div className="col_1 w-[20%]">
+                <Button variant="text" className='!text-black gap-2 w-full'><RiMenu2Line className='text-[18px]'/>
+                Shop by Categories <LiaAngleDownSolid className='text-[13px] ml-auto'/>
+                </Button>
+            </div>
+            <div className="col_2 w-[60%]">
+                <ul className='flex items-center gap-4'>
+                    <li className='list-none'>
+                        <Link to="/" className='link transition text-[14px] font-[500]'> 
+                        <Button className='link transition !font-[500] !text-[rgba(0,0,0,0.8)]
+                        hover:!text-[#ff5252]'> Home </Button> 
+                        </Link>
+                    </li>
+                    <li className='list-none'>
+                        <Link to="/" className='link transition text-[14px] font-[500]'> 
+                        <Button className='link transition !font-[500] !text-[rgba(0,0,0,0.8)]
+                        hover:!text-[#ff5252]'>Fashion </Button>
+                        </Link>
+                    </li>
+                    <li className='list-none'>
+                        <Link to="/" className='link transition text-[14px] font-[500]'> 
+                        <Button className='link transition !font-[500] !text-[rgba(0,0,0,0.8)]
+                        hover:!text-[#ff5252]'>Electronics </Button></Link>
+                    </li>
+                    <li className='list-none'>
+                        <Link to="/" className='link transition text-[14px] font-[500]'> 
+                        <Button className='link transition !font-[500] !text-[rgba(0,0,0,0.8)]
+                        hover:!text-[#ff5252]'>Bags</Button> 
+                        </Link>
+                    </li>
+                    <li className='list-none'>
+                        <Link to="/" className='link transition text-[14px] font-[500]'> 
+                        <Button className='link transition !font-[500] !text-[rgba(0,0,0,0.8)]
+                        hover:!text-[#ff5252]'> Footwear </Button>
+                        </Link>
+                    </li>
+                    <li className='list-none'>
+                        <Link to="/" className='link transition text-[14px] font-[500]'> 
+                        <Button className='link transition !font-[500] !text-[rgba(0,0,0,0.8)]
+                        hover:!text-[#ff5252]'> Groceries </Button>
+                        </Link>
+                    </li>
+                    <li className='list-none'>
+                        <Link to="/" className='link transition text-[14px] font-[500]'> 
+                        <Button className='link transition !font-[500] !text-[rgba(0,0,0,0.8)]
+                        hover:!text-[#ff5252]'> Beauty </Button>
+                        </Link>
+                    </li>
+                    <li className='list-none'>
+                        <Link to="/" className='link transition text-[14px] font-[500]'> 
+                        <Button className='link transition !font-[500] !text-[rgba(0,0,0,0.8)]
+                        hover:!text-[#ff5252]'> Wellness </Button>
+                        </Link>
+                    </li>
+                    <li className='list-none'>
+                        <Link to="/" className='link transition text-[14px] font-[500]'> 
+                        <Button className='link transition !font-[500] !text-[rgba(0,0,0,0.8)]
+                        hover:!text-[#ff5252]'> Jewellery </Button>
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+            <div className="col_3 w-[20%]">
+                <p className='text-[14px] font-[500] flex items-center gap-3 mb-0 mt-0'>
+                    <GoRocket className='text-[18px]' />
+                    Free International Delivery
+                </p>
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Navigation
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,14 +2,33 @@ import React from 'react'
 import { Link } from "react-router-dom" 
 import logo from "../../assets/logo.jpg"
 import Search from '../Search'
-import Button from '@mui/material/Button';
+import Badge from '@mui/material/Badge';
+import { styled } from '@mui/material/styles';
+import IconButton from '@mui/material/IconButton';
+import { IoCartOutline } from "react-icons/io5";
+import { IoIosGitCompare } from "react-icons/io";
+import { FaRegHeart } from "react-icons/fa";
+import Tooltip from '@mui/material/Tooltip';
+import Navigation from './Navigation';
+
+
+
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  '& .MuiBadge-badge': {
+    right: -3,
+    top: 13,
+    border: `2px solid ${theme.palette.background.paper}`,
+    padding: '0 4px',
+  },
+}));
 
 
 const Header = () => {
   return (
     <div>
-      <header>
-        <div className="top-strip py-2 border-t-[1px] border-gray-250 border-b-[1px]">
+      <header className='bg-white'>
+        <div className="top-strip py-2 border-t-[1px] border-gray-300 border-b-[1px]">
             <div className="container">
                 <div className="flex items-center justify-between">
                     <div className="col1 w-[50%]">
@@ -36,7 +55,7 @@ const Header = () => {
             </div>
         </div>
 
-        <div className="header py-3">
+        <div className="header py-4 border-b-[1px] border-gray-300">
             <div className="container flex items-center justify-between">
                 <div className="col1 w-[25%]">
                     <Link to={"/"}><img src={logo} alt="" /></Link>
@@ -45,16 +64,45 @@ const Header = () => {
                     <Search/>
                 </div>
                 <div className="col3 w-[30%] flex items-center pl-7">
-                    <ul className='flex items-center gap-3'>
+                    <ul className='flex items-center justify-end gap-3 w-full'>
                         <li className='list-none'>
                             <Link to="/login" className='link transition text-[15px] font-[500]'>Login</Link>
                              &nbsp; | &nbsp;
                             <Link to="/register" className='link transition text-[15px] font-[500]'> Register</Link>
                         </li>
+                        <li>
+                            <Tooltip title="Cart">
+                            <IconButton aria-label="cart">
+                                <StyledBadge badgeContent={4} color="secondary">
+                                    <IoCartOutline />
+                                </StyledBadge>
+                            </IconButton>
+                            </Tooltip>
+                        </li>
+                         <li>
+                            <Tooltip title="WishList">
+                            <IconButton aria-label="WishList">
+                                <StyledBadge badgeContent={4} color="secondary">
+                                    <FaRegHeart />
+                                </StyledBadge>
+                            </IconButton>
+                            </Tooltip>
+                        </li>
+                        <li>
+                            <Tooltip title="Compare">
+                            <IconButton aria-label="Compare">
+                                <StyledBadge badgeContent={4} color="secondary">
+                                    <IoIosGitCompare />
+                                </StyledBadge>
+                            </IconButton>
+                            </Tooltip>
+                        </li>
                     </ul>
                 </div>
             </div>
         </div>
+
+            <Navigation/>
       </header>
     </div>
   )
